e3ad6c29488313109c2037ccbc68051116a4fc0e
coppernium <joaovitordelima81@usp.br>
2024-02-03 22:46:32 -0300

    Fiz uma pasta para meu projeto da formação do sistema solar

---
+++ Mechanics/BallRotationalFlatness.py
@@ -0,0 +1,6 @@
+from vpython import *
+canvas(width=1900, height=930, background=color.black)
+
+# Constantes
+R = 1
+esfera = ellipsoid(pos=vec(0,0,0), size=vec(2,1,1), color=color.cyan)
---
+++ Mechanics/RuthefordScattering.py
@@ -0,0 +1,6 @@
+from vpython import *
+canvas(width=1900, height=930, background=color.black)
+
+# ball colision test
+ball1 = sphere(pos=vec(10,0,0), radius=1, color=color.yellow)
+ball1 = sphere(pos=vec(0,0,0), radius=1, color=color.red)
